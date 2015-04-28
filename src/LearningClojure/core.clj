983ec16f26921b7aa27e549e67a2e562be291af6
PRS <Phil.R.Simmons@gmail.com>
2015-04-28 15:44:43 -0400

    Added a little bit to figure out imports

--- src/LearningClojure/core.clj
+++ src/LearningClojure/core.clj
@@ -1,11 +1,19 @@
 (ns LearningClojure.core)
+(require '[clojure.string :as string])
+
+;Checks if the argument is in the set of variable characters
 (def vowel? (set "aeiou"))
 
+;Takes a word and converts it to pig latin
 (defn pig-latin [word]
+  ;For the first let of a word
   (let [first-letter (first word)]
+    ;if it is a vowel
     (if (vowel? first-letter)
+      ;return the word with ay at the back
       (str word "ay")
+      ; return the word minus the last letter, the first letter and then ay
       (str (subs word 1) first-letter "ay"))))
 
-(println (pig-latin "red"))
-(println (pig-latin "orange"))
+(println (string/capitalize (pig-latin "red")))
+(println (string/capitalize (pig-latin "orange")))
