0cc88333d258e7272ec0b8f61bf68ac2c70ad0ce
PRS <Phil.R.Simmons@gmail.com>
2015-04-28 15:12:52 -0400

    Started using Lein with Clojure and created trivial test program

---
+++ src/LearningClojure/core.clj
@@ -0,0 +1,11 @@
+(ns LearningClojure.core)
+(def vowel? (set "aeiou"))
+
+(defn pig-latin [word]
+  (let [first-letter (first word)]
+    (if (vowel? first-letter)
+      (str word "ay")
+      (str (subs word 1) first-letter "ay"))))
+
+(println (pig-latin "red"))
+(println (pig-latin "orange"))
