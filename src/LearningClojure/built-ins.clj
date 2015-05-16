6b1050e4483f000dd8d64a6cfd272bae71d0ce90
prs <Phil.R.Simmons@gmail.com>
2015-05-16 11:44:02 -0400

    Added some collection learning examples

--- src/LearningClojure/built-ins.clj
+++ src/LearningClojure/built-ins.clj
@@ -1,6 +1,14 @@
+(require '[clojure.string :as string])
 (def testmap
   (hash-map :green :blue))
 (println  (testmap :green))
 
 (def stooges (vector "Test" "Test2"))
-(println (stooges 1))
+(println (stooges 0))
+(println (count stooges))
+(def rev_stooges (reverse stooges) )
+(println rev_stooges)
+;(println (rev_stooges 0))
+(println (first stooges))
+(println (last stooges))
+(println (map string/upper-case stooges))
